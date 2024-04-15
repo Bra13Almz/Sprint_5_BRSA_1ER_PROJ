ac9a56f29699edb1e0d632c107fde2b0769c7209
Bra Almz <odiseo1310@gmail.com>
2024-04-15 20:55:15 +1000

    creacion de aplicacion

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Sprint_5_BRSA_1ER_PROJ
 Proyecto final del sprint 5 
+
+Creación de 2 graficos y su posterior deploy.
---
+++ app1_blsa.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+
+
+st.title('Mi primer app')
+
+car_data = pd.read_csv('vehicles_us.csv')
+
+hist_button = st.button('Construir histogramas') 
+
+if hist_button:
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    fig = px.histogram(car_data, x="odometer")
+    st.plotly_chart(fig, use_container_width=True)
+
+
+disp_button = st.button('Construir gráfico de dispersión') 
+
+if disp_button:
+    st.write('Creación de gráfico de dispersion para el conjunto de datos de anuncios de venta de coches')
+    fig = px.scatter(car_data, x="odometer", y="price")
+    st.plotly_chart(fig, use_container_width=True)
+
+
+
+
+
+
+
+
